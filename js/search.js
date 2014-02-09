652e0e9e2cb3bb2bfee779d8f628be81d46e7a9b
klan <klnie1989@gmail.com>
2014-02-09 22:15:30 +0100

    Added search.js

---
+++ js/search.js
@@ -0,0 +1,60 @@
+jQuery(document).ready(function ($) {
+  // on submit
+  $('#searchform').submit(function(event) {
+    event.preventDefault();
+
+    // setting variables
+    $query = $("#search").val();
+    $url = 'http://itunes.apple.com/search';
+
+    // validation
+    if ($query.trim() && $query != 'Search') {
+      $.ajax({
+        type: "POST",
+        url: $url,
+        data: {term:$query, media:"music", entity:"musicArtist,album"},
+        dataType: "jsonp",
+      }).done(function(json) {
+        // console.log(json.results);
+        // empty result element
+        $('#result').empty();
+        $.each(json.results, function(key, value) {
+          // creating list element
+          $li = $('<li></li>');
+
+          // creating entry
+          $entry = $('<div></div>');
+          $entry.addClass('entry');
+
+          // artist name
+          $artistName = $('<a></a>');
+          $artistName.attr('href', value.artistLinkUrl || value.artistViewUrl);
+          $artistName.html(value.artistName);
+
+          // collection name
+          $collectionName = $('<a></a>');
+          $collectionName.attr('href', value.collectionViewUrl);
+          $collectionName.html(value.collectionName);
+
+          // appending to entry
+          $entry.append($artistName, $collectionName);
+
+          // creating details
+          $details = $('<div></div>');
+          $details.addClass('details');
+
+          // wrapper type
+          $details.html('wrapperType: ' + value.wrapperType);
+
+          // appending to list element
+          $li.append($entry, $details);
+          // appending to result element
+          $('#result').append($li);
+        });
+      }).fail(function(json) {
+        // appending to result element
+        $('#result').append('<h1>FAIL</h1>');
+      });
+    }
+  });
+});
