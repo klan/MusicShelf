3b1a43904073b7194ba54d52c084a7a088c9d15f
klan <klnie1989@gmail.com>
2015-03-23 22:36:24 +0100

    Minor changes Changed some markup, some styling, removed a party crasher and changes a couple of selectors

--- js/resizable.js
+++ js/resizable.js
@@ -1,28 +1,30 @@
 jQuery(function($) {
 
   // setting vars
-  var column01 = $('#column01');
-  var column02 = $('#column02');
+  var left = $('#left');
+  var right = $('#right');
   var windowWidth = $(window).width();
 
   // setting init style
-  column01.css({
-    'width': windowWidth/2,
-    'max-width': windowWidth - 350
+  left.css({
+    'width': (windowWidth / 2) < 350 ? 350 : windowWidth / 2,
+    'max-width': (windowWidth - 350) < 350 ? 350 : windowWidth - 350
   });
-  column02.css({
-    'width': windowWidth/2,
-    'max-width': windowWidth - 350
+  right.css({
+    'width': (windowWidth / 2) < 350 ? 350 : windowWidth / 2,
+    'max-width': (windowWidth - 350) < 350 ? 350 : windowWidth - 350
   });
 
   // update vars and style on resize
   $(window).resize(function() {
     windowWidth = $(window).width();
-    column01.css({
-      'width': windowWidth - column02.width()
+
+    left.css({
+      'width':  (windowWidth - right.width()) < 350 ? 350 : windowWidth - right.width(),
+      'max-width': (windowWidth - 350) < 350 ? 350 : windowWidth - 350
     });
-    column02.css({
-      'max-width': windowWidth - 350
+    right.css({
+      'max-width': (windowWidth - 350) < 350 ? 350 : windowWidth - 350
     });
   });
 
@@ -30,13 +32,13 @@ jQuery(function($) {
     $(document).on('mousemove', function(event) {
       event.preventDefault();
 
-      column01.css({
+      left.css({
         // limiting to min and max width
         'width':  event.pageX < 350 ? 350 :
                   event.pageX > (windowWidth - 350) ? windowWidth - 350 :
                   event.pageX
       });
-      column02.css({
+      right.css({
         // limiting to min and max width
         'width':  (windowWidth - event.pageX) < 350 ? 350 :
                   (windowWidth - event.pageX) > (windowWidth - 350) ? windowWidth - 350 :
@@ -47,7 +49,6 @@ jQuery(function($) {
     // removing event handlers
     $(document).on('mouseup', function() {
       $(document).off('mouseup').off('mousemove');
-      $('#handle').off('mousedown');
     });
   });
 
